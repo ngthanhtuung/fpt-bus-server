bb4045723bd9c51edfd2998f445eb8fda762132c
Tung Nguyen <tungnt16092001@gmail.com>
2023-02-13 23:19:37 +0700

    Prepare authen middleware

--- src/middlewares/auth/verify-token.middleware.js
+++ src/middlewares/auth/verify-token.middleware.js
@@ -1,10 +1,22 @@
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-// const authenticate = (req, res, next) => {};
+const authenticate = (req, res, next) => {
+  const token = req.header("accessToken");
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decode;
+    next();
+  } catch (error) {
+    res.status(401).send({
+      message: "You are not logged into the system",
+    });
+  }
+};
 
-// const authorize = (req, res, next) => {};
+const authorize = (req, res, next) => {};
 
-// module.exports = {
-//   authenticate,
-//   authorize,
-// };
+module.exports = {
+  authenticate,
+  authorize,
+};
