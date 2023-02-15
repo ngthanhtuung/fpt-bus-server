da55db35d97084fb90b96da06dd1ffcf53227ebe
Tung Nguyen <tungnt16092001@gmail.com>
2023-02-16 00:08:32 +0700

    Add field profile_img in table Users

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-const app = require('./app')
+const app = require("./app");
 
 app.listen(process.env.PORT || 8888, () => {
-    console.log(`Server is running at http://${process.env.HOST || localhost}:${process.env.PORT || 8888}`);
-    console.log(`Swagger is running at http://${process.env.HOST || localhost}:${process.env.PORT || 8888}/api-docs`)
-})
+  console.log(
+    `Server is running at http://${process.env.HOST || "localhost"}:${
+      process.env.PORT || 8888
+    }`
+  );
+  console.log(
+    `Swagger is running at http://${process.env.HOST || "localhost"}:${
+      process.env.PORT || 8888
+    }/api-docs`
+  );
+});
