6d3e9c0b6bfea761a8b166daf2b5d4ef8d746887
Tung Nguyen <tungnt16092001@gmail.com>
2023-02-17 13:04:07 +0700

    fix getUsers swagger

--- src/config/docs/routers/api/getUsers.docs.js
+++ src/config/docs/routers/api/getUsers.docs.js
@@ -2,6 +2,15 @@ module.exports = {
   get: {
     tags: ["Users"],
     description: "API for managing users",
+    parameters: {
+      key: {
+        name: "key",
+        in: "path",
+        description: "Key of redis",
+        required: true,
+        type: "string",
+      },
+    },
     responses: {
       200: {
         description: "Get all user successfully",
