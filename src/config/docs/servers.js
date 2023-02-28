d1a572a5ff15363295f2b93d20e5d2cccdc9d973
Tung Nguyen <tungnt16092001@gmail.com>
2023-03-01 00:27:19 +0700

    reject a server

--- src/config/docs/servers.js
+++ src/config/docs/servers.js
@@ -1,10 +1,10 @@
 module.exports = {
   servers: [
-    {
-      url: `http://${process.env.HOST || localhost}:${
-        process.env.PORT || 8888
-      }`,
-    },
+    // {
+    //   url: `http://${process.env.HOST || localhost}:${
+    //     process.env.PORT || 8888
+    //   }`,
+    // },
     {
       url: `http://fpt-bus.online`,
     },
