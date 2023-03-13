9d1289c919c3e97e05df0c095d211e05ee2d0d09
BanhQuyDev <BanhQuyDev>
2023-03-14 01:41:58 +0700

    done sign in by phone number driver

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -10,77 +10,54 @@ const createAccessToken = (payload) => {
 
 const signIn = async (req, res) => {
   try {
-    const { phone, accessToken, methodVerify } = req.body
-    if (accessToken) {
-      const googlePayload = jwt.decode(
-        accessToken,
-        process.env.FIREBASE_SECRET
-      );
+    const { accessToken } = req.body
+    const googlePayload = jwt.decode(
+      accessToken,
+      process.env.FIREBASE_SECRET
+    );
 
-      const userLogin = await Users.findOne({
-        where: {
-          email: googlePayload.email,
-          status: true,
+    const userLogin = await Users.findOne({
+      where: {
+        email: googlePayload.email,
+        status: true,
+      },
+      include: [
+        {
+          model: RoleTypes,
+          attributes: ["role_name"],
         },
-        include: [
-          {
-            model: RoleTypes,
-            attributes: ["role_name"],
-          },
-        ],
+      ],
+    });
+    if (userLogin) {
+      const payload = {
+        id: userLogin.id,
+        fullname: userLogin.fullname,
+        email: userLogin.email,
+        phone_number: userLogin.phone_number,
+        student_id: userLogin.student_id,
+        profile_img: userLogin.profile_img,
+        status: userLogin.status,
+        role_name: userLogin.RoleType.role_name,
+      };
+      const accessToken = createAccessToken(payload);
+      res.status(200).json({
+        status: "Success",
+        messages: "Login successfully!",
+        data: {
+          user: payload,
+          accessToken,
+        },
       });
-      if (userLogin) {
-        const payload = {
-          id: userLogin.id,
-          fullname: userLogin.fullname,
-          email: userLogin.email,
-          phone_number: userLogin.phone_number,
-          student_id: userLogin.student_id,
-          profile_img: userLogin.profile_img,
-          status: userLogin.status,
-          role_name: userLogin.RoleType.role_name,
-        };
-        const accessToken = createAccessToken(payload);
-        res.status(200).json({
-          status: "Success",
-          messages: "Login successfully!",
-          data: {
-            user: payload,
-            accessToken,
-          },
-        });
-      } else {
-        checkEmailDomain(googlePayload.email, ["fpt.edu.vn"])
-          ? signUp(req, res)
-          : res.status(400).json({
-            status: "Fail",
-            messages:
-              "Please contact your administrator to support your account!",
-          });
-      }
     } else {
-      if (phone) {
-        client.verify.v2.services(process.env.SERVICE_ID).verifications.create({
-          to: `+84${phone}`,
-          channel: methodVerify === 'call' ? 'call' : 'sms'
-        })
-          .then(data => {
-            res.status(200).send({
-              status: "Success",
-              messages: "Verification is sent!!",
-              Phone: phone,
-              data
-            })
-          })
-      } else {
-        res.status(400).send({
+      checkEmailDomain(googlePayload.email, ["fpt.edu.vn"])
+        ? signUp(req, res)
+        : res.status(400).json({
           status: "Fail",
-          messages: "Wrong phone number!!!",
-          Phone: phone,
-          data
-        })
-      }
+          messages:
+            "Please contact your administrator to support your account!",
+        });
     }
+
   } catch (err) {
     res.status(500).json({
       status: "Fail",
@@ -140,8 +117,140 @@ const signUp = async (req, res) => {
     });
   }
 };
-
+//Sign driver
+const signInPhoneNumber = async (req, res) => {
+  try {
+    const { phone, methodVerify } = req.body
+    if (phone == undefined) {
+      return res.status(400).json({
+        status: "Fail",
+        messages:
+          "Not enough params !!",
+      });
+    }
+    const userLogin = await Users.findOne({
+      where: {
+        phone_number: phone,
+        status: true,
+      },
+      include: [
+        {
+          model: RoleTypes,
+          attributes: ["role_name"],
+        },
+      ],
+    });
+    if (userLogin == undefined) {
+      return res.status(400).json({
+        status: "Fail",
+        messages:
+          "Your phone number is not registered! Please contact your administrator to support your account!",
+      });
+    }
+    if (phone) {
+      client.verify.v2.services(process.env.SERVICE_ID).verifications.create({
+        to: `+84${phone}`,
+        channel: methodVerify === 'call' ? 'call' : 'sms'
+      })
+        .then(data => {
+          res.status(200).send({
+            status: "Success",
+            messages: "Verification is sent!!",
+            Phone: phone,
+            data
+          })
+        })
+    } else {
+      res.status(400).send({
+        status: "Fail",
+        messages: "Wrong phone number!!!",
+        Phone: phone,
+        data
+      })
+    }
+  } catch (err) {
+    res.status(500).json({
+      status: "Fail",
+      messages: err.message,
+    });
+  }
+};
+//Verify code phone number
+const verifyCodeNumber = async (req, res) => {
+  try {
+    const { phone, code } = req.body
+    console.log("Phone:", phone);
+    console.log("Code:", code);
+    //check param
+    if (code != undefined && phone != undefined) {
+      //check phone number đã được đăng kí chưa ?
+      const userLogin = await Users.findOne({
+        where: {
+          phone_number: phone,
+          status: true,
+        },
+        include: [
+          {
+            model: RoleTypes,
+            attributes: ["role_name"],
+          },
+        ],
+      });
+      console.log("userLogin:", userLogin);
+      if (userLogin != undefined) {
+        const payload = {
+          id: userLogin.id,
+          fullname: userLogin.fullname,
+          email: userLogin.email,
+          phone_number: userLogin?.phone_number,
+          student_id: userLogin.student_id,
+          profile_img: userLogin.profile_img,
+          status: userLogin.status,
+          role_name: userLogin.RoleType.role_name,
+        };
+        //check mã code để verify phone number
+        client.verify.v2.services(process.env.SERVICE_ID)
+          .verificationChecks
+          .create({ to: `+84${phone}`, code })
+          .then(verification_check => {
+            console.log(verification_check.status)
+            if (verification_check.status === "approved") {
+              const accessToken = createAccessToken(payload);
+              res.status(200).json({
+                status: "Success",
+                messages: "Login successfully!",
+                data: {
+                  user: payload,
+                  accessToken,
+                },
+              });
+            }
+          });
+      } else {
+        res.status(400).json({
+          status: "Fail",
+          messages:
+            "Your phone number is not registered! Please contact your administrator to support your account!",
+        });
+      }
+    } else {
+      res.status(400).send({
+        status: "Fail",
+        messages: "Not enough params!!!",
+        Phone: phone,
+        data
+      })
+    }
+  } catch (err) {
+    res.status(500).json({
+      status: "Fail",
+      messages: err.message,
+    });
+  }
+};
 module.exports = {
   signIn,
   signUp,
+  signInPhoneNumber,
+  verifyCodeNumber
 };
--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
-const { signIn } = require("../controllers/auth.controller");
+const { signIn, signInPhoneNumber, verifyCodeNumber } = require("../controllers/auth.controller");
 
 const authRouter = Router();
 
 authRouter.post("/sign-in", signIn);
+authRouter.post("/phone/sign-in", signInPhoneNumber);
+authRouter.post("/verify-phone", verifyCodeNumber);
 
 module.exports = authRouter;
